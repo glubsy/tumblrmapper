fa699ea138c0c739ff3d37e9698df75cc5a57acd
glubsy <glubsy@users.noreply.github.com>
2018-04-24 22:06:07 +0200

    clearer paths, isinstance fix

--- update_archive_lists.py
+++ update_archive_lists.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 #
-# Build the file list of archived files for which we need to recover their _raw 
+# Build the file list of archived files for which we need to recover their _raw
 #
 # Generates archives_files_list_minus_todelete.txt which is a result of:
 #   archives_1280_set.txt
@@ -19,7 +19,7 @@ import re
 
 # tumblr_base_noext_re = re.compile(r'(tumblr_.*)_.{3,4}.*') # tumblr_xxx_r1
 # tumblr_base_norev_noext_re = re.compile(r'(tumblr_.*)(?:_r\d)?_.{3,4}.*')
-# exclude revision 
+# exclude revision
 tumblr_base_norev_noext_nongreedy_re = re.compile(r'(tumblr_(?:inline_)?.*?)(?:_r\d)?_.{3,4}.*')
 
 
@@ -55,16 +55,16 @@ and (FILE_NAME NOT SIMILAR TO 'tumblr\_%\_raw.%' escape '\');
 
 def remove_raw_alternatives_from_1280(list_1280, list_raw, slow=False):
     """Returns a set of difference"""
-    if not isinstance(set, type(list_1280)):
-        if not "autogen" in list_1280 and isinstance(str, type(list_1280)):
+    if not isinstance(set(), type(list_1280)):
+        if not "cache" in list_1280 and isinstance(str(), type(list_1280)):
             _list_1280 = readfile(list_1280)
         else:   # is string path to pickle
             _list_1280 = readfile_pickle(list_1280)
     else:       # must be a set()
         _list_1280 = list_1280
 
-    if not isinstance(set, type(list_raw)):
-        if not "autogen" in list_raw and isinstance(str, type(list_raw)):
+    if not isinstance(set(), type(list_raw)):
+        if not "cache" in list_raw and isinstance(str(), type(list_raw)):
             _raw_set = readfile(list_raw)
         else:   # is string path to pickle
             _raw_set = readfile_pickle(list_raw)
@@ -118,7 +118,7 @@ def remove_todelete_from_list(current_1280_set, todelete_txt, slow=False):
         todelete_set = readfile(todelete_txt)
 
         for item in todelete_set:
-            match = tumblr_base_norev_noext_re.match(item)
+            match = tumblr_base_norev_noext_nongreedy_re.match(item)
             if match:
                 reresult = match.group(1)
                 for item in current_1280_set:
@@ -134,7 +134,7 @@ def remove_todelete_from_list(current_1280_set, todelete_txt, slow=False):
         # print("doing difference between current_set: {0} and todelete_set: {1}"
         # .format(len(current_1280_set), len(todelete_set)))
         return current_1280_set.difference(todelete_txt)
-    
+
 
 
 def readfile_pickle(filepath):
@@ -150,7 +150,7 @@ def readfile(filepath):
     """Returns a set of each line in file"""
 
     fileset = set()
-    
+
     with open(filepath, 'r') as f:
         for line in f:
             fileset.add(line)
@@ -173,21 +173,26 @@ def write_to_file(filepath, mylist, use_pickle=False):
 
 if __name__ == "__main__":
     SCRIPTDIR = os.path.dirname(__file__)
+    debug = True
 
     # _1280 files listing
-    archives_1280_txt_path = SCRIPTDIR + os.sep + "tools/archives_files_list.txt"
-    archives_1280_txt_autogen = SCRIPTDIR + os.sep + "tools/archives_files_list_autogen.txt" #caching
+    archives_1280_txt_path = SCRIPTDIR + os.sep + "tools/archives_1280_list.txt"
+    archives_1280_txt_cache = SCRIPTDIR + os.sep + "tools/archives_1280_cache.txt" # caching
+    archives_1280_set_debug = SCRIPTDIR + os.sep + "tools/archives_1280_set_debug.txt" # debug
 
     # _raw files listing
-    raw_downloads_txt_path = SCRIPTDIR + os.sep + "tools/raw_archives.txt"
-    raw_downloads_txt_autogen = SCRIPTDIR + os.sep + "tools/raw_archives_autogen.txt" #caching
+    raw_downloads_txt_path = SCRIPTDIR + os.sep + "tools/downloads_raw_list.txt"
+    raw_downloads_txt_cache = SCRIPTDIR + os.sep + "tools/downloads_raw_cache.txt" #caching
+    raw_downloads_set_debug = SCRIPTDIR + os.sep + "tools/downloads_raw_set_debug.txt" #caching
 
     # optional todelete files (filter out)
     to_delete_archives_txt = os.path.expanduser("~/Documents/DOCUMENTS/archive_files_to_delete.txt")
-    
-    # final filtered file output
-    archives_files_list_minus_todelete_path = SCRIPTDIR + os.sep + "tools/archives_files_list_minus_todelete.txt"
 
+    # debug
+    archives_minus_raw_path = SCRIPTDIR + os.sep + "tools/archives_minus_raw.txt"
+
+    # final filtered file output
+    archives_files_list_minus_raw_minus_todelete_path = SCRIPTDIR + os.sep + "tools/archives_files_list_minus_raw_minus_todelete.txt"
 
     # current downloads _raw
     downloads_database = db_handler.Database(db_filepath="/home/firebird/downloads.vvv", \
@@ -201,21 +206,21 @@ if __name__ == "__main__":
     raw_downloads_set = None
 
     if not os.path.exists(archives_1280_txt_path):
-        if not os.path.exists(archives_1280_txt_autogen):
+        if not os.path.exists(archives_1280_txt_cache):
             archives_1280_set = get_1280_from_db(CGI_database)
             print("length of archives_1280_set: {}".format(len(archives_1280_set)))
-            write_to_file(archives_1280_txt_autogen, archives_1280_set, use_pickle=True)
-            # DEBUG
-            # write_to_file(SCRIPTDIR + os.sep + "tools/archives_1280_set.txt", archives_1280_set, use_pickle=False)
+            write_to_file(archives_1280_txt_cache, archives_1280_set, use_pickle=True)
+            if debug:
+                write_to_file(archives_1280_set_debug, archives_1280_set, use_pickle=False)
 
     # fetch _raw files from DB if text file doesn't already exist
     if not os.path.exists(raw_downloads_txt_path):
-        if not os.path.exists(raw_downloads_txt_autogen):
+        if not os.path.exists(raw_downloads_txt_cache):
             raw_downloads_set = get_raw_files_from_db(downloads_database)
             print("length of raw_downloads_set: {}".format(len(raw_downloads_set)))
-            write_to_file(raw_downloads_txt_autogen, raw_downloads_set, use_pickle=True)
-            # DEBUG
-            # write_to_file(SCRIPTDIR + os.sep + "tools/raw_downloads_set.txt", raw_downloads_set, use_pickle=False)
+            write_to_file(raw_downloads_txt_cache, raw_downloads_set, use_pickle=True)
+            if debug:
+                write_to_file(raw_downloads_set_debug, raw_downloads_set, use_pickle=False)
 
 
     if raw_downloads_set is not None:
@@ -224,8 +229,8 @@ if __name__ == "__main__":
         else: # use path
             if os.path.exists(archives_1280_txt_path):
                 archives1280_minus_raw_set = remove_raw_alternatives_from_1280(archives_1280_txt_path, raw_downloads_set)
-            elif os.path.exists(archives_1280_txt_autogen):
-                archives1280_minus_raw_set = remove_raw_alternatives_from_1280(archives_1280_txt_autogen, raw_downloads_set)
+            elif os.path.exists(archives_1280_txt_cache):
+                archives1280_minus_raw_set = remove_raw_alternatives_from_1280(archives_1280_txt_cache, raw_downloads_set)
             else:
                 print("ERROR: no 1280 listing found!")
                 sys.exit(0)
@@ -235,27 +240,27 @@ if __name__ == "__main__":
             if os.path.exists(raw_downloads_txt_path):
                 archives1280_minus_raw_set = remove_raw_alternatives_from_1280(archives_1280_set, raw_downloads_txt_path)
                 print(archives1280_minus_raw_set)
-            elif os.path.exists(raw_downloads_txt_autogen):
-                archives1280_minus_raw_set = remove_raw_alternatives_from_1280(archives_1280_set, raw_downloads_txt_autogen)
+            elif os.path.exists(raw_downloads_txt_cache):
+                archives1280_minus_raw_set = remove_raw_alternatives_from_1280(archives_1280_set, raw_downloads_txt_cache)
             else:
                 print("ERROR: no raw listing found!")
         else: # we have no 1280 set nor raw set
             if os.path.exists(archives_1280_txt_path) and os.path.exists(raw_downloads_txt_path):
                 archives1280_minus_raw_set = remove_raw_alternatives_from_1280(archives_1280_txt_path, raw_downloads_txt_path)
-            elif os.path.exists(archives_1280_txt_autogen) and os.path.exists(raw_downloads_txt_path):
-                archives1280_minus_raw_set = remove_raw_alternatives_from_1280(archives_1280_txt_autogen, raw_downloads_txt_path)
-            elif os.path.exists(archives_1280_txt_autogen) and os.path.exists(raw_downloads_txt_autogen):
-                archives1280_minus_raw_set = remove_raw_alternatives_from_1280(archives_1280_txt_autogen, raw_downloads_txt_autogen)
+            elif os.path.exists(archives_1280_txt_cache) and os.path.exists(raw_downloads_txt_path):
+                archives1280_minus_raw_set = remove_raw_alternatives_from_1280(archives_1280_txt_cache, raw_downloads_txt_path)
+            elif os.path.exists(archives_1280_txt_cache) and os.path.exists(raw_downloads_txt_cache):
+                archives1280_minus_raw_set = remove_raw_alternatives_from_1280(archives_1280_txt_cache, raw_downloads_txt_cache)
             else:
                 print("ERROR: no 1280 listing nor raw listing found!")
 
     print("archives_minus_raw_set: {0}".format(len(archives1280_minus_raw_set)))
-    # DEBUG
-    write_to_file(SCRIPTDIR + os.sep + "tools/archives_minus_raw_set.txt", archives1280_minus_raw_set, use_pickle=False)
+    write_to_file(archives_minus_raw_path, archives1280_minus_raw_set, use_pickle=False)
 
 
     # archives1280_final = archives1280_minus_raw - to_delete_archives_txt
     if os.path.exists(to_delete_archives_txt):
-        archives_files_list_minus_todelete = remove_todelete_from_list(archives1280_minus_raw_set, to_delete_archives_txt)
-        print("archives_files_list_minus_todelete: {0}".format(len(archives_files_list_minus_todelete)))
-        write_to_file(archives_files_list_minus_todelete_path, archives_files_list_minus_todelete)
+        archives_files_list_minus_raw_minus_todelete = remove_todelete_from_list(archives1280_minus_raw_set, to_delete_archives_txt)
+        print("archives_files_list_minus_raw_minus_todelete: {0}".format(len(archives_files_list_minus_raw_minus_todelete)))
+        write_to_file(archives_files_list_minus_raw_minus_todelete_path, archives_files_list_minus_raw_minus_todelete)
+
