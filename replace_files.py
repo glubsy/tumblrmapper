380455fd2690932f6116d624cb119d85d5cde418
glubsy <glubsy@users.noreply.github.com>
2018-07-04 18:36:12 +0200

    Finding _raw corresponding to _1280

--- replace_files.py
+++ replace_files.py
@@ -9,8 +9,10 @@ import sys
 import argparse
 import re
 import shutil
+from constants import BColors
 
-tumblr_base_noext_re = re.compile(r'(tumblr_(?:inline_|messaging_)?.*?(?:_r\d)?)_\d{3,4}.*') # tumblr_xxx_r1_1280
+tumblr_base_noext_re = re.compile(r'(tumblr_(?:inline_|messaging_)?.*?(?:_r\d)?)_(\d{3,4})\..*') # tumblr_xxx_r1_1280
+tumblr_base_noext_raw_re = re.compile(r'(tumblr_(?:inline_|messaging_)?.*?(?:_r\d)?)_raw\..*') # tumblr_xxx_r1_raw
 
 def parse_args():
     """Parse command-line arguments."""
@@ -21,16 +23,16 @@ def parse_args():
     parser.add_argument('-o', '--output_dir', action="store", type=str, default=os.getcwd(),
     help="Directory where files will be moved to temporarily for double check.")
 
-    parser.add_argument('-s', '--ref_dir', action="store", type=str, default="/test/CGI",
+    parser.add_argument('-s', '--ref_dir', action="store", type=str, default=os.path.expanduser("~/test/CGI"),
     help="Directory where _raw files are stored. Can be set as same as input_dir.")
-    
+
     parser.add_argument('-d', '--delete_mode', action="store_true", default=False,
     help="Move out files found in to_delete_list")
     parser.add_argument('-r', '--replace_files', action="store", default=True,
     help="Move _raw files in place of _1280")
 
 
-    parser.add_argument('-t', '--to_delete_list', action="store", 
+    parser.add_argument('-t', '--to_delete_list', action="store",
     default="/home/nupupun/Documents/DOCUMENTS/tumblr_archives/archive_files_to_delete.txt",
     help="path to the list of files to delete.")
 
@@ -53,13 +55,27 @@ def read_deletion_list(filepath):
     return _set
 
 
+def find(name, path):
+    """Returns the first occurence of name in path"""
+    print(f"find({name})")
+    for root, dirs, files in os.walk(path):
+        for _file in files:
+            _file = _file.split('.')[0]
+            if name in _file:
+                return os.path.join(root, name)
 
-def walk_directory(_dir, filter=None):
+
+def walk_directory(_dir, suffix_filter=None):
     """Returns list of tuples [(file's dir path, file's parent dir name, filename)]"""
     filelist = []
     for root, dirs, files in os.walk(_dir):
         for _file in files:
-            filelist.append((root, os.path.basename(root), _file))
+            if suffix_filter == "raw":
+                match = tumblr_base_noext_raw_re.match(_file)
+                if match:
+                    filelist.append((root, os.path.basename(root), _file))
+            else:
+                filelist.append((root, os.path.basename(root), _file))
     return filelist
 
 
@@ -69,15 +85,15 @@ def main():
     input_dir = args.input_dir
     if args.output_dir is not None:
         output_dir = args.output_dir
-    
+
     if args.to_delete_list is not None:
         to_delete_list =  read_deletion_list(args.to_delete_list)
         # print(f"to_delete_list:\n{to_delete_list}")
         print(f"to_delete_list length: {len(to_delete_list)}")
 
     if os.path.isdir(args.ref_dir):
-        raw_file_list = walk_directory(args.ref_dir, filter="raw")
-
+        raw_file_list = walk_directory(args.ref_dir, suffix_filter="raw")
+        print(f"raw_file_list: {raw_file_list}")
 
     subdir_list = []
     for root, dirs, files in os.walk(input_dir):
@@ -93,15 +109,23 @@ def main():
             # test if to_delete
             if to_delete_list is not None:
                 if triple[2].split('.')[0] in to_delete_list:
-                    print(f"File {triple[2]} is marked for deletion.")
+                    # print(f"File {triple[2]} is marked for deletion.")
                     if args.delete_mode:
-                        print(f"Moving file to {args.output_dir}")
+                        print(f"{BColors.LIGHTYELLOW}Moving {triple[2]} to trash:{BColors.ENDC} {args.output_dir}")
 
 
-            # test if tumblr 1280
+            # test if tumblr 1280/500/250 etc.
             match = tumblr_base_noext_re.match(triple[2])
             if match:
-                print(f"found {triple[2]} as match")
+                print(f"Found resized: {triple[2]}")
+                # look for corresponding _raw in _raw file list
+                # removing extension, and resized suffix from name
+                filename = triple[2].split('.')[0]
+                filename = re.split(match.group(2), filename)[0]
+                filename = filename + 'raw'
+                found_corresponding_raw = find(filename, args.ref_dir)
+                if found_corresponding_raw:
+                    print(f"{BColors.GREENOK}Found {found_corresponding_raw} corresponding to {triple[2]}{BColors.ENDC}")
 
 
 
